b6db764b2652ce9722e3323e805716e7ba358ff9
Louis Dyer <louisalexanderdyer@gmail.com>
2023-06-27 12:59:07 +1200

    Run migrations successfully

---
+++ server/db/migrations/20230627005531_items.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('items', (table) => {
+    table.increments('id').primary()
+    table.string('item')
+    table.integer('list_id')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('items')
+}
--- server/db/seeds/items.js
+++ server/db/seeds/items.js
@@ -2,15 +2,15 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('items').del()
   await knex('items').insert([
-    { id: 1, item: 'Make it prettier', listId: 1 },
-    { id: 2, item: 'Add auth', listId: 1 },
-    { id: 3, item: 'Add confetti animations', listId: 1 },
-    { id: 4, item: 'Add Next.js routes', listId: 2 },
-    { id: 5, item: 'Deploy', listId: 2 },
-    { id: 6, item: 'Write tests', listId: 2 },
-    { id: 7, item: 'Backlog', listId: 3 },
-    { id: 8, item: 'Build FE components', listId: 3 },
-    { id: 9, item: 'Add drag and drop', listId: 3 },
-    { id: 10, item: 'Make the database', listId: 4 },
+    { id: 1, item: 'Make it prettier', list_id: 1 },
+    { id: 2, item: 'Add auth', list_id: 1 },
+    { id: 3, item: 'Add confetti animations', list_id: 1 },
+    { id: 4, item: 'Add Next.js routes', list_id: 2 },
+    { id: 5, item: 'Deploy', list_id: 2 },
+    { id: 6, item: 'Write tests', list_id: 2 },
+    { id: 7, item: 'Backlog', list_id: 3 },
+    { id: 8, item: 'Build FE components', list_id: 3 },
+    { id: 9, item: 'Add drag and drop', list_id: 3 },
+    { id: 10, item: 'Make the database', list_id: 4 },
   ])
 }
