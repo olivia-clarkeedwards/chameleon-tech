db58b8a022b53541a85efa29e1c73f84de9b21cf
Louis Dyer <louisalexanderdyer@gmail.com>
2023-06-27 12:39:09 +1200

    Add seeds

---
+++ server/db/seeds/lists.js
@@ -0,0 +1,10 @@
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('lists').del()
+  await knex('lists').insert([
+    { id: 1, list: 'Backlog' },
+    { id: 2, list: 'Doing' },
+    { id: 3, list: 'Review' },
+    { id: 4, list: 'Done' },
+  ])
+}
